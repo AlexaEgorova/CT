0ffcca0af0a250f246903ef6a2688395458c42d4
AlexaEgorova <37927173+AlexaEgorova@users.noreply.github.com>
2019-03-28 21:41:47 +0300

    Update xlmsThroughDirs.cpp

--- xlmsThroughDirs.cpp
+++ xlmsThroughDirs.cpp
@@ -13,54 +13,51 @@ using namespace cv;
 using namespace std;
 
 
-void fileTypes(string filename) {
+void fileTypes(string filename, map<string, int>& typesCount) {
 	FileStorage fs(filename, FileStorage::READ);
 	FileNode node = fs["FrameDataArray"];
 	string txt;
-	//map<string, int> typesCount;
 	for (auto it : node) {
 		FileNode subNode = it["FrameObjects"];
 		for (auto subIt : subNode) {
 			txt = (string)subIt["type"];
 			typesCount[txt] += 1;
 		}
-	}
-
-	
+	}	
 }
 
 
 int main(int args, char* argv[]) {
 	if (args == 1) {
 		cout << "No path specified!" << endl;
-	} else {
-		string path;
-		path = argv[1];
-		struct stat s;
-		char *cpath = &path[0u];
-		if (stat(cpath, &s) < 0) {
-			cout << "No such file or directory: " << path << endl;
-		}
-		else {
-			map<string, int> typesCount;
-			if (s.st_mode & S_IFDIR != 0) {
-				vector<String> paths;
-				glob(path + "/*markup.xml", paths, true);
-				for (auto &filename : paths) {
-					cout << filename << endl;
-					fileTypes(filename, typesCount);
-				}
-			}
-			else {
-				if (path.substr(path.length() - 4, 4) == ".xml") {
-					fileTypes(path, typesCount);
-				}
-				else cout << "Not a xml file: " << path << endl;
-			}
-			for (auto &i : typesCount) {
-				cout << i.first << " : " << i.second << endl;
-			}
+		return -1;
+	} 
+	string path;
+	path = argv[1];
+	struct stat s;
+	char *cpath = &path[0u];
+	if (stat(cpath, &s) < 0) {
+		cout << "No such file or directory: " << path << endl;
+		return -1;
+	}
+	map<string, int> typesCount;
+	if (s.st_mode & S_IFDIR != 0) {
+		vector<String> paths;
+		glob(path + "/*markup.xml", paths, true);
+		for (auto &filename : paths) {
+			cout << filename << endl;
+			fileTypes(filename, typesCount);
 		}
 	}
+	else {
+		if (path.substr(path.length() - 4, 4) == ".xml") {
+			fileTypes(path, typesCount);
+		}
+		else cout << "Not a xml file: " << path << endl;
+	}
+	for (auto &i : typesCount) {
+		cout << i.first << " : " << i.second << endl;
+	}
+	
 	return 0;
 }
